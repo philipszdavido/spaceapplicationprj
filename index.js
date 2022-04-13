b1b9b40c2a0c2507744e6fb762b3be46d6bb3245
philipszdavido <kurtwanger40@gmail.com>
2022-04-13 15:04:08 +0100

    refactor

--- index.js
+++ index.js
@@ -1,23 +1,5 @@
 const fs = require("fs");
 
-// read the csv file
-const csv = fs.readFileSync("exercise_input.csv", "utf8");
-
-const result = csv
-  .trim()
-  .split("\n")
-  .map((line) => {
-    const lines = line.trim().split(",");
-    const orderedItems = orderItems(lines.slice(3));
-    return orderedItems.map((i) => {
-      return {
-        voter: lines[1],
-        votes: +i.slice(2),
-        item: i.slice(0, 2),
-      };
-    });
-  });
-
 function orderItems(items) {
   return items
     .sort((a, b) => {
@@ -28,9 +10,32 @@ function orderItems(items) {
     .reverse();
 }
 
-result.forEach((item) => {
-  item.forEach((i) => {
-    console.log(`Voter: "${i.voter}", Item: "${i.item}", Votes: "${i.votes}"`);
+function main() {
+  // read the csv file
+  const csv = fs.readFileSync("exercise_input.csv", "utf8");
+
+  const result = csv
+    .trim()
+    .split("\n")
+    .map((line) => {
+      const lines = line.trim().split(",");
+      const orderedItems = orderItems(lines.slice(3));
+      return orderedItems.map((i) => {
+        return {
+          voter: lines[1],
+          votes: +i.slice(2),
+          item: i.slice(0, 2),
+        };
+      });
+    });
+  result.forEach((item) => {
+    item.forEach((i) => {
+      console.log(
+        `Voter: "${i.voter}", Item: "${i.item}", Votes: "${i.votes}"`
+      );
+    });
+    console.log("----------------------------------------");
   });
-  console.log("----------------------------------------");
-});
+}
+
+main();
