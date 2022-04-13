a0d1ed8050685d35c80071c7435d8066c46218cc
philipszdavido <kurtwanger40@gmail.com>
2022-04-13 16:10:25 +0100

    made changes

--- index.js
+++ index.js
@@ -11,31 +11,41 @@ function orderItems(items) {
 }
 
 function main() {
+  const items = {};
   // read the csv file
   const csv = fs.readFileSync("exercise_input.csv", "utf8");
 
-  const result = csv
+  csv
     .trim()
     .split("\n")
-    .map((line) => {
+    .forEach((line) => {
       const lines = line.trim().split(",");
       const orderedItems = orderItems(lines.slice(3));
-      return orderedItems.map((i) => {
-        return {
-          voter: lines[1],
-          votes: +i.slice(2),
-          item: i.slice(0, 2),
-        };
+      orderedItems.forEach((i) => {
+        const itemName = i.slice(0, 2);
+        //  check if item is in items
+        if (items[itemName]) {
+          items[itemName].votes += +i.slice(2);
+          items[itemName].voters.push(lines[1]);
+        } else {
+          items[itemName] = {
+            votes: +i.slice(2),
+            voters: [lines[1]],
+          };
+        }
       });
     });
 
-  result.forEach((item) => {
-    item.forEach((i) => {
-      console.log(
-        `Voter: "${i.voter}", Item: "${i.item}", Votes: "${i.votes}"`
-      );
-    });
-    console.log("----------------------------------------");
+  const sorted = Object.entries(items)
+    .sort((a, b) => {
+      return b[1].votes - a[1].votes;
+    })
+    .slice(1);
+
+  sorted.forEach((item) => {
+    console.log(
+      `Item: ${item[0]}, Votes: ${item[1].votes}, Voters: ${item[1].voters}`
+    );
   });
 }
 
